8f3ecf5fc0bd031e9bc77075ade2206a380cdcb6
Peter Baker <b.tarde@gmail.com>
2020-08-02 13:07:59 -0400

    Options to convert to and from compact syntax

--- Python/xgridfit.py
+++ Python/xgridfit.py
@@ -12,17 +12,21 @@ import argparse
 
 # First read the command-line arguments. At minimum we need the inputfile.
 
-argparser = argparse.ArgumentParser(description='Compile XML into TrueType instructions.')
+argparser = argparse.ArgumentParser(description='Compile XML into TrueType instructions, embedded in a Python script.')
 argparser.add_argument("inputfile", help='Xgridfit (XML) file to process.')
 argparser.add_argument("outputfile", nargs='?', help="Name of Python script to output")
 argparser.add_argument('--novalidation', action="store_true", help="Skip validation of the input file")
 argparser.add_argument('--nocompilation', action="store_true", help="Skip compilation of the input file")
+argparser.add_argument('--expand', action="store_true", help="Convert file to expanded syntax, save, and exit")
+argparser.add_argument('--compact', action="store_true", help="Convert file to compact syntax, save, and exit")
 args = argparser.parse_args()
 
-inputfile  = args.inputfile
-outputfile = args.outputfile
-skipval    = args.novalidation
-skipcomp   = args.nocompilation
+inputfile   = args.inputfile
+outputfile  = args.outputfile
+skipval     = args.novalidation
+skipcomp    = args.nocompilation
+expandonly  = args.expand
+compactonly = args.compact
 
 # Get the xgridfit file.
 
@@ -42,20 +46,44 @@ if len(xgffile.xpath("/xg:xgridfit/xi:include", namespaces=ns)):
     
 have_cvar = (len(xgffile.xpath("/xg:xgridfit/xg:cvar", namespaces=ns)) > 0)
 
-# Next determine whether we are using long tagnames or short. Best way
-# is to find out which tag is used for the required <pre-program> (<prep>)
-# element. If we don't find it, print an error message and exit.
-
 # Look for all program files relative to this python file:
 # xsl files in ../XSL/, schemas in ../Schemas.
 progpath = os.path.split(os.path.dirname(__file__))[0]
 
-# If we're using compact syntax, expand it.
+# Next determine whether we are using long tagnames or short. Best way
+# is to find out which tag is used for the required <pre-program> (<prep>)
+# element. If we don't find it, print an error message and exit.
+
+# If the file uses compact syntax, always expand it.
 if len(xgffile.xpath("/xg:xgridfit/xg:prep", namespaces=ns)):
-    expandprog = etree.parse(progpath + "/XSL/expand.xsl")
-    etransform = etree.XSLT(expandprog)
+    etransform = etree.XSLT(etree.parse(progpath + "/XSL/expand.xsl"))
     xgffile = etransform(xgffile)
-elif not(len(xgffile.xpath("/xg:xgridfit/xg:pre-program", namespaces=ns))):
+    if expandonly:
+        tstr = str(xgffile)
+        tstr = tstr.replace('xgf:','')
+        tstr = tstr.replace('xmlns:xgf="http://xgridfit.sourceforge.net/Xgridfit2"','')
+        if outputfile:
+            of = open(outputfile, "w")
+            of.write(tstr)
+            of.close()
+        else:
+            print(tstr)
+        sys.exit(0)
+elif len(xgffile.xpath("/xg:xgridfit/xg:pre-program", namespaces=ns)):
+    if compactonly:
+        etransform = etree.XSLT(etree.parse(progpath + "/XSL/compact.xsl"))
+        xgffile = etransform(xgffile)
+        tstr = str(xgffile)
+        tstr = tstr.replace('xgf:','')
+        tstr = tstr.replace('xmlns:xgf="http://xgridfit.sourceforge.net/Xgridfit2"','')
+        if outputfile:
+            of = open(outputfile, "w")
+            of.write(tstr)
+            of.close()
+        else:
+            print(tstr)
+        sys.exit(0)
+else:
     print("The xgridfit program must contain a pre-program (prep) element,")
     print("even if it's empty.")
     sys.exit(1)
@@ -67,11 +95,11 @@ elif not(len(xgffile.xpath("/xg:xgridfit/xg:pre-program", namespaces=ns))):
 if skipval:
     print("# Skipping validation")
 else:
-    xmlschemadoc = etree.parse(progpath + "/Schemas/xgridfit.xsd")
-    xmlschema = etree.XMLSchema(xmlschemadoc)
+    # xmlschemadoc = etree.parse(progpath + "/Schemas/xgridfit.xsd")
+    xmlschema = etree.XMLSchema(etree.parse(progpath + "/Schemas/xgridfit.xsd"))
     xmlschema.assertValid(xgffile)
 
-# Transform the xgridfit program to generate Python output.
+# Now compile.
     
 if skipcomp:
     print("# Skipping compilation")
