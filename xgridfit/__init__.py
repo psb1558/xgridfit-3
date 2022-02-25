4f60e44a09c15a3f500de0c4f23eaca67b23abf1
Peter Baker <b.tarde@gmail.com>
2022-02-24 23:40:40 -0500

    Added imports

--- xgridfit/__init__.py
+++ xgridfit/__init__.py
@@ -1,6 +1,7 @@
 from .xgridfit import (main, get_file_path, wipe_font, install_glyph_program,
                        compact_instructions, install_cvt, install_functions,
-                       install_prep, install_cvar, validate)
+                       install_prep, install_cvar, validate, make_coordinate_index,
+                       coordinates_to_points)
 from .version import __version__
 
 __author__  = "Peter S. Baker"
